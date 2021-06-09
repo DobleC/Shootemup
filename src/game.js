193cb76ac9a6e456d3bc8a3604381562d46a1bdf
DobleC <rexitah@gmail.com>
2021-06-09 15:39:08 +0200

    Añadido psd de los PUPs y comentado el gradiente

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 var game = {
-    bgGradient: null,
+    //bgGradient: null,
     player: null,
     enemies: [],
     powerups: [],
@@ -9,14 +9,15 @@ var game = {
 
     Start: function()
     {
-        // prepare the bg gradient
+        /*// prepare the bg gradient
         this.bgGradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
         this.bgGradient.addColorStop(1, "rgb(112, 162, 218)");
         this.bgGradient.addColorStop(0.9494949494949495, "rgb(0, 122, 255)");
         this.bgGradient.addColorStop(0.4292929292929293, "rgb(38, 71, 108)");
         this.bgGradient.addColorStop(0.16161616161616163, "rgb(0, 0, 0)");
         this.bgGradient.addColorStop(0, "rgb(1, 1, 1)");
-    
+        */
+
         // player is a global variable
         this.player = player;
         this.player.start(new Vector2(canvas.width / 2, canvas.height - 60));
